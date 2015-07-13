6102841ab34f07b7d23acbb6a356675cc011b3e0
shuhei <kawashu0723@gmail.com>
2015-07-13 19:19:25 +0900

    create DB and insert userData

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -1,11 +1,23 @@
 package controllers
 
-import "github.com/revel/revel"
+import (
+	"fmt"
+
+	"iidaPro/app/models"
+
+	"github.com/revel/revel"
+)
 
 type App struct {
 	*revel.Controller
 }
 
 func (c App) Index() revel.Result {
+	rows, _ := DbMap.Select(models.User{}, "select * from user")
+	for _, row := range rows {
+		user := row.(*models.User)
+		fmt.Printf("%d, %s, %s\n", user.Id, user.Name, user.HashedPassword)
+	}
+
 	return c.Render()
 }
